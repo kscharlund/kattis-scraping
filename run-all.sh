a7a40f2577a5c3bfd2c8b1ed709c747b4a52fdd3
Karl Schärlund <kalle@scharlund.se>
2021-01-08 21:42:35 +0100

    Add call to write_to_html

--- run-all.sh
+++ run-all.sh
@@ -4,3 +4,6 @@ cd $(dirname $0)
 python fetch_problems.py
 python sample_problems.py
 python send_to_slack.py
+if [ -d /home/www/maxflow.org ]; then
+    python write_to_html.py $(date "+%Y-%m-%d") > /home/www/maxflow.org/potw.html
+fi
